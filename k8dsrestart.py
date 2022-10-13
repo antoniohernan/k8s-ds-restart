4bd2ce62aa54454c4712166d756245743d134193
antoniohernan <antonio.hernan@gmail.com>
2022-10-13 11:47:22 +0200

    Partial develop

--- k8dsrestart.py
+++ k8dsrestart.py
@@ -1,19 +1,31 @@
 from kubernetes import client, config
+import kubernetes.client
+from kubernetes.client.rest import ApiException
+import time
 import argparse
+import json
 
 # Arguments parser
 parser = argparse.ArgumentParser(description='Restart daemon set pods in all nodes')
 parser.add_argument('--namespace',required=True, help='Namespace name in which the Daemonset is deployed')
 parser.add_argument('--daemonset',required=True, help='Daemoset name')
 args = parser.parse_args()
-
-print ("\n\n Pods running in DaemonSet: %s deployed in the NameSpace: %s\n" % (args.namespace, args.daemonset))
+pretty = 'true'
 
 # Configs can be set in Configuration class directly or using helper utility
 config.load_kube_config()
 
 v1 = client.CoreV1Api()
-print("Listing pods with their IPs:")
-ret = v1.list_pod_for_all_namespaces(watch=False)
+
+print ("\n\n Pods running in DaemonSet: %s deployed in the NameSpace: %s\n" % (args.namespace, args.daemonset))
+
+try:
+  ret = v1.list_namespaced_pod(args.namespace,pretty=pretty)
+except ApiException as e:
+  print("Exception when calling CoreV1Api->list_namespaced_pod: %s\n" % e)
+
+#print ("%s\n" % ret.items[0])
+
+print("%s\t%s\t%s" % ('Pod Name','Run Node','Status'))
 for i in ret.items:
-    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+    print("%s\t%s\t%s" % (i.metadata.name, i.spec.node_name, i.status.phase))
