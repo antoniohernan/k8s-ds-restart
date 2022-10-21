7c5477abef705939c7949b40c1f1e5d5f4f9247c
antoniohernan <antonio.hernan@gmail.com>
2022-10-21 14:14:55 +0200

    Test delete pod

--- k8dsrestart.py
+++ k8dsrestart.py
@@ -24,9 +24,34 @@ try:
 except ApiException as e:
   print("Exception when calling CoreV1Api->list_namespaced_pod: %s\n" % e)
 
-#print ("%s\n" % ret.items[1])
+lpods = []
 
 print("%s\t%s\t%s\t\t%s" % ('Pod Name','Run Node','Status','Container Ready'))
 for i in ret.items:
     print("%s\t%s\t%s\t\t%s" % (i.metadata.name, i.spec.node_name, i.status.phase, i.status.container_statuses[0].ready))
+    lpods.append(i.spec.node_name+";"+i.metadata.name)
 
+#print ("Contenido de lista pods #:", lpods)
+
+for i in lpods:
+  element    = i.split(";")
+  nodeworker = element[0]
+  podname    = element[1]
+  fail = 0
+  print ("\n Restarting pod: %s in node: %s " % (podname, nodeworker), end='')
+  retrycount = 0
+  while True:
+    retrycount += 1
+    print(".", end='',flush=True)
+    body = client.V1DeleteOptions()
+    try:
+      v1.delete_namespaced_pod(podname, args.namespace, body=body)
+    except ApiException as x:
+      print("Exception when calling CoreV1Api->delete_namespaced_pod: %s\n" % x)
+    time.sleep(2)
+#    if retrycount >= 10:
+#      print(" fail")
+#      fail = 2
+#      break
+  if fail == 0:
+    print(" ok")
